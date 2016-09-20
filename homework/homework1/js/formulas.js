c632b34af3b241a8a666edab97d00547c622f3e7
Ines Swenson <ines.koch2@gmail.com>
2016-09-20 12:23:49 -0700

    Changed docstring, created helper function to round number to certain decimal and fixed syntax error.

--- homework/homework1/js/formulas.js
+++ homework/homework1/js/formulas.js
@@ -1,45 +1,51 @@
+/*
+* Homework solution
+* 
+* 
+*/
 
-// Instructions for your homework
-// //////////////////////////////////////////////////////////////////
-// 1.   Here is where your functions should be defined
-// 2.	What should you name your functions?  Hint:  check the console to see which functions are already being called.  Are they all "defined?"  If not yet defined... then define them here!
-// 3.	Be sure to link up this file in your HTML doc
-/////////////////////////////////////////////////////////////////////
-
-//		Questions: In Assignment Math.random is listed, 
-//		but doesnt seem to be required in the solution unless
-//		a default value is required
-
-//		Research for me: JS function description best practice
-
-function calcFahrenheitToCelcius(fTemp){}
+function calcFahrenheitToCelcius (fTemp){
 	// Converts Fahrenheit into Celcius
-	// params: fTemp | number
-	// return: cTemp | number
-	var cTemp = Math.round((fTemp - 32) * 5 / 9);
+	// @param num - fTemp
+	// @return num - cTemp 
+	var cTemp = (fTemp - 32) * 5 / 9;
+	cTemp = Math.round(cTemp);
 	return cTemp;
 }
 
-function calcCelciusToFarenheit(cTemp){
-	//Converts Celcius into Fahrenheit
-	// params: cTemp | number
-	// return: fTemp | number
-	var fTemp = Math.round(cTemp * 9 / 5 + 32); 
+function calcCelciusToFarenheit (cTemp){
+	// Converts Celcius into Fahrenheit
+	// @param num - cTemp
+	// @return num - fTemp 
+	var fTemp = cTemp * 9 / 5 + 32;
+	fTemp = Math.round(fTemp);
 	return fTemp
 }
 
-function calcCircumference(radius){
-	// Converts radius in circumference
-	// params: radius | number
-	// return: circ | number
+function calcCircumference (radius){
+	// Converts radius in circumference with 1 decimal precision
+	// @param num - radius
+	// @return num - circ
 	var circ = Math.PI * 2 * radius;
+	circ = roundWithDec(circ, 1)
 	return circ;
 }
 
-function calcLongestSide(a, b){
-	// Calculates the length of the hypothenusis of a triange
-	// params: length a, length b | number
-	// return: length c | number
+function calcLongestSide (a, b){
+	// Calculates the length of the hypotenuse of a triange
+	// @param num - a
+	// @param num - b
+	// @return num - c
 	var c = Math.sqrt((a * a) + (b * b));
+	c = roundWithDec(c, 1);
 	return c;
+}
+
+function roundWithDec (number, dec){
+	// Rounds number and fixes to decimal
+	// @param num - number
+	// @param num - dec
+	// @return num - rounded and fixed number
+	var decFactor = Math.pow (10, dec);
+	return Math.round(number*decFactor)/decFactor;
 }
